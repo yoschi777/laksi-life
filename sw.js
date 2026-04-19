8ff01274b5c5ddf11a124f963744fc5931ad2c42
yoschi777 <laksi.yacine@gmail.com>
2026-04-19 04:31:58 +0200

    fix: network-first SW so app auto-updates

--- sw.js
+++ sw.js
@@ -1,12 +1,25 @@
-const CACHE_NAME = 'laksi-life-v1';
-const urlsToCache = ['./index.html'];
+const CACHE_NAME = 'laksi-life-v3';
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
